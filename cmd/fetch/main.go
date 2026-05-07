0f1c2ab326fda4a8400a76cb0fe4f8854541a3fb
Cole Styron <colestyron@gmail.com>
2026-05-07 13:17:04 -0800

    converts to loop-based backfill

--- cmd/fetch/main.go
+++ cmd/fetch/main.go
@@ -3,61 +3,119 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/cstyr/weather/internal/ambient"
 	"github.com/cstyr/weather/internal/store"
 )
 
+type EnvConfig struct {
+	macAddr     string
+	apiKey      string
+	appKey      string
+	region      string
+	tableName   string
+	stationName string
+}
+
 func main() {
+	envConfig, err := loadEnvConfig()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	client := &ambient.Client{
+		APIKey: envConfig.apiKey,
+		AppKey: envConfig.appKey,
+	}
+
+	store, err := store.New(envConfig.region, envConfig.tableName)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	var endDate int64
+
+	for {
+		results, err := client.FetchPage(envConfig.macAddr, endDate, 288)
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+
+		// fmt.Printf("Results: %+v\n", results)
+		recordCount := len(results)
+		if recordCount == 0 {
+			break
+		}
+		fmt.Printf("fetched %d records", recordCount)
+
+		err = store.WriteRecords(envConfig.stationName, results)
+		if err != nil {
+			fmt.Println("error writing records:", err)
+			return
+		}
+
+		latestResult := results[recordCount-1].DateUTC
+
+		fmt.Println("oldest timestamp: ", latestResult)
+
+		endDate = latestResult - 1
+
+		// weather API is rate limited to one request per second
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Println("DONE")
+
+}
+
+func loadEnvConfig() (*EnvConfig, error) {
+
 	macAddr, ok := os.LookupEnv("AMBIENT_DEVICE_MAC")
 	if !ok {
-		fmt.Println("mac address not set")
+		err := fmt.Errorf("mac address not set")
+		return nil, err
 	}
 
 	apiKey, ok := os.LookupEnv("AMBIENT_API_KEY")
 	if !ok {
-		fmt.Println("api key not set")
+		err := fmt.Errorf("api key not set")
+		return nil, err
 	}
 
 	appKey, ok := os.LookupEnv("AMBIENT_APP_KEY")
 	if !ok {
-		fmt.Println("app key not set")
+		err := fmt.Errorf("app key not set")
+		return nil, err
 	}
 
 	region, ok := os.LookupEnv("AWS_REGION")
 	if !ok {
-		fmt.Println("region not set")
+		err := fmt.Errorf("region not set")
+		return nil, err
 	}
 
 	tableName, ok := os.LookupEnv("WEATHER_TABLE_DDB")
 	if !ok {
-		fmt.Println("tablename not set")
+		err := fmt.Errorf("tablename not set")
+		return nil, err
 	}
 
-	client := &ambient.Client{
-		APIKey: apiKey,
-		AppKey: appKey,
+	stationName, ok := os.LookupEnv("STATION_NAME")
+	if !ok {
+		err := fmt.Errorf("station not set")
+		return nil, err
 	}
 
-	results, err := client.FetchPage(macAddr, 0, 1)
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
-
-	fmt.Printf("Results: %+v\n", results)
-
-	store, err := store.New(region, tableName)
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
-
-	err = store.WriteRecords("112thFabShop_1", results)
-	if err != nil {
-		fmt.Println("error writing records:", err)
-		return
-	}
-	fmt.Println("DONE")
-
+	return &EnvConfig{
+		macAddr:     macAddr,
+		apiKey:      apiKey,
+		appKey:      appKey,
+		region:      region,
+		tableName:   tableName,
+		stationName: stationName,
+	}, nil
 }
